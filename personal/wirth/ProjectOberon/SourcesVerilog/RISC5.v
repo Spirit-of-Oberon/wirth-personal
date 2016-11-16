d863118395dcaec7f9b3a312019d66d44ee914f4
κρμγ <petryxa.clever@gmail.com>
2016-11-16 23:55:43 +0300

    упдатэ

--- personal/wirth/ProjectOberon/SourcesVerilog/RISC5.v
+++ personal/wirth/ProjectOberon/SourcesVerilog/RISC5.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps  // 13.9.2016
+`timescale 1ns / 1ps  // 9.11.2016
 
 module RISC5(
 input clk, rst, stallX,
@@ -21,7 +21,7 @@ wire [21:0] pcmux, nxpc;
 wire cond, S;
 wire sa, sb, sc;
 
-wire p, q, u, v, w;  // instruction fields
+wire p, q, u, v;  // instruction fields
 wire [3:0] op, ira, ira0, irb, irc;
 wire [2:0] cc;
 wire [15:0] imm;
@@ -30,19 +30,18 @@ wire [21:0] disp;
 
 wire regwr;
 wire stall, stallL, stallM, stallD, stallFA, stallFM, stallFD;
-wire [1:0] sc1, sc0;  // shift counts
-
 wire a0, a1, a2, a3;
 wire [7:0] inbusL, outbusB0, outbusB1, outbusB2, outbusB3;
 wire [23:0] inbusH;
 
 wire [31:0] A, B, C0, C1, aluRes, regmux;
-wire [31:0] s1, s2, s3, t1, t2, t3;
+wire [31:0] lshout, rshout;
 wire [31:0] quotient, remainder;
 wire [63:0] product;
 wire [31:0] fsum, fprod, fquot;
 
-wire ADD, SUB, MUL, DIV; wire FAD, FSB, FML, FDV;
+wire ADD, SUB, MUL, DIV;
+wire FAD, FSB, FML, FDV;
 wire LDR, STR, BR;
 
 PROM PM (.adr(pcmux[8:0]), .data(pmout), .clk(clk));
@@ -52,6 +51,10 @@ Multiplier mulUnit (.clk(clk), .run(MUL), .stall(stallM),
 
 Divider divUnit (.clk(clk), .run(DIV), .stall(stallD),
    .u(~u), .x(B), .y(C1), .quot(quotient), .rem(remainder));
+
+LeftShifter LSUnit (.x(B), .y(lshout), .sc(C1[4:0]));
+
+RightShifter RSUnit(.x(B), .y(rshout), .sc(C1[4:0]), .md(ins[16]));
 
 FPAdder fpaddx (.clk(clk), .run(FAD|FSB), .u(u), .v(v), .stall(stallFA),
    .x(B), .y({FSB^C0[31], C0[30:0]}), .z(fsum));
@@ -67,7 +70,6 @@ assign p = ins[31];
 assign q = ins[30];
 assign u = ins[29];
 assign v = ins[28];
-assign w = ins[16];
 assign cc  = ins[26:24];
 assign ira = ins[27:24];
 assign irb = ins[23:20];
@@ -97,32 +99,10 @@ assign C0 = R[irc];
 // Arithmetic-logical unit (ALU)
 assign ira0 = BR ? 15 : ira;
 assign C1 = q ? {{16{v}}, imm} : C0;
-// assign adr = stallL ? B[23:0] + {4'b0, off} : {pcmux, 2'b00};
 assign adr = stallL ? B[23:0] + {{4{off[19]}}, off} : {pcmux, 2'b00};
 assign rd = LDR & ~stallX & ~stall1;
 assign wr = STR & ~stallX & ~stall1;
 assign ben = p & ~q & v & ~stallX & ~stall1;  // byte enable
-
-assign sc0 = C1[1:0];
-assign sc1 = C1[3:2];
-
-// shifter for ASR and ROR
-assign s1 = (sc0 == 3) ? {(w ? B[2:0] : {3{B[31]}}), B[31:3]} :
-    (sc0 == 2) ? {(w ? B[1:0] : {2{B[31]}}), B[31:2]} :
-    (sc0 == 1) ? {(w ? B[0] : B[31]), B[31:1]} : B;
-assign s2 = (sc1 == 3) ? {(w ? s1[11:0] : {12{s1[31]}}), s1[31:12]} :
-    (sc1 == 2) ? {(w ? s1[7:0] : {8{s1[31]}}), s1[31:8]} :
-    (sc1 == 1) ? {(w ? s1[3:0] : {4{s1[31]}}), s1[31:4]} : s1;
-assign s3 = C1[4] ? {(w ? s2[15:0] : {16{s2[31]}}), s2[31:16]} : s2;
-
-// shifter for LSL
-assign t1 = (sc0 == 3) ? {B[28:0], 3'b0} :
-    (sc0 == 2) ? {B[29:0], 2'b0} :
-    (sc0 == 1) ? {B[30:0], 1'b0} : B;
-assign t2 = (sc1 == 3) ? {t1[19:0], 12'b0} :
-    (sc1 == 2) ? {t1[23:0], 8'b0} :
-    (sc1 == 1) ? {t1[27:0], 4'b0} : t1;
-assign t3 = C1[4] ? {t2[15:0], 16'b0} : t2;
 
 assign aluRes =  // 21.71 ns
   ~op[3] ?
@@ -132,8 +112,8 @@ assign aluRes =  // 21.71 ns
           (q ?  // MOV
             (~u ? {{16{v}}, imm} : {imm, 16'b0}) :
             (~u ? C0 : (~v ? H : {N, Z, C, OV, 20'b0, 8'h50}))) :
-          t3) :  //  LSL
-        s3) : //  ASR, ROR
+          lshout) :  //  LSL
+        rshout) : //  ASR, ROR
       (~op[1] ?
         (~op[0] ? B & C1 : B & ~C1) :  // AND, ANN
         (~op[0] ? B | C1 : B ^ C1))) : // IOR. XOR
