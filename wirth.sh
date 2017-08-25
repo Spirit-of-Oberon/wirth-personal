12a8e7da74f7b739f5e4a68282d2cfdca6df6176
Пётр <petryxa.clever@gmail.com>
2017-08-25 23:30:25 +0300

    update + wget script update

--- wirth.sh
+++ wirth.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-wget -r -k -l 7 -p -E -np https://www.inf.ethz.ch/personal/wirth/
+wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5" -r -k -l 7 -p -E -np https://www.inf.ethz.ch/personal/wirth/
