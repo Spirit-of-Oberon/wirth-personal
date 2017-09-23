4987492cbdf84d4f7a4d0c5ceb01c7b612fc6e98
pk <petryxa.clever@gmail.com>
2017-09-23 12:07:18 +0100

    upd

--- wirth.sh
+++ wirth.sh
@@ -1,2 +1,8 @@
 #!/bin/sh 
-wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5" -r -k -l 7 -p -E -np https://www.inf.ethz.ch/personal/wirth/
+wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5" -r -k -l 7 -p -E -np 'https://www.inf.ethz.ch/personal/wirth/'
+
+cd www.inf.ethz.ch 
+
+git commit -a -m "upd"
+
+git push
