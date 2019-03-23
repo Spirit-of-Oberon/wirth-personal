13714d4e42bd9e501070d52b9ff32896bd4592bf
kpmy <petryxa.clever@gmail.com>
2019-03-23 15:04:54 +0100

    upd

--- wirth.sh
+++ wirth.sh
@@ -5,7 +5,7 @@ git pull
 
 cd ..
 
-wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5" -r -k -l 7 -p -E -np 'https://www.inf.ethz.ch/personal/wirth/'
+wget --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5" -r -k -l 7 -p -E -np 'https://people.inf.ethz.ch/wirth/'
 
 cd www.inf.ethz.ch 
 
